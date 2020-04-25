1c95e3b9b652e098fac7f1be3cada0448839ba7a
A248 <anandebeh@gmail.com>
2020-04-25 08:40:02 -0400

    Add Importer and ImportSource

---
+++ importer/src/main/java/space/arim/swiftconomy/importer/Importer.java
@@ -0,0 +1,42 @@
+/* 
+ * SwiftConomy-importer
+ * Copyright © 2020 Anand Beh <https://www.arim.space>
+ * 
+ * SwiftConomy-importer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * SwiftConomy-importer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with SwiftConomy-importer. If not, see <https://www.gnu.org/licenses/>
+ * and navigate to version 3 of the GNU General Public License.
+ */
+package space.arim.swiftconomy.importer;
+
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Consumer;
+
+import space.arim.swiftconomy.api.SwiftConomy;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public abstract class Importer {
+	
+	private final SwiftConomy economy;
+	private final ImportSource<?> source;
+	private final Consumer<Map<UUID, Long>> asyncAcceptor;
+	
+	public CompletableFuture<?> start() {
+		source.link();
+		return source.convertBalances(economy.getArithmetic()).thenAcceptAsync(asyncAcceptor).thenRun(source::unlink);
+	}
+	
+}
